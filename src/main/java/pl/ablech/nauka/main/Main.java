4a902484e0c6aa59f2c401ac553f5b9e305b707e
ablech <gargantua@interia.pl>
2016-12-07 12:59:44 +0100

    Main file to testing added

---
+++ src/main/java/pl/ablech/nauka/main/Main.java
@@ -0,0 +1,26 @@
+package pl.ablech.nauka.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import pl.ablech.nauka.entities.Student;
+import pl.ablech.nauka.utils.HibernateUtils;
+
+/**
+ * Created by Arek on 07.12.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Andrew");
+        student.setSurname("Student");
+        student.setDeansGroup("AI1001");
+
+        SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(student);
+        session.getTransaction().commit();
+
+        sessionFactory.close();
+    }
+}
--- src/main/java/pl/ablech/nauka/utils/HibernateUtils.java
+++ src/main/java/pl/ablech/nauka/utils/HibernateUtils.java
@@ -1,7 +1,32 @@
 package pl.ablech.nauka.utils;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 /**
  * Created by Arek on 07.12.2016.
  */
 public class HibernateUtils {
+    private static SessionFactory sessionFactory;
+
+    private static SessionFactory buildSessionFactory(){
+        try{
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .configure().build();
+            return new Configuration().buildSessionFactory(serviceRegistry);
+        }
+        catch(Throwable ex){
+            throw new ExceptionInInitializerError(ex);
+        }
+
+    }
+
+    public static SessionFactory getSessionFactory(){
+        if(sessionFactory ==null)
+            sessionFactory = buildSessionFactory();
+        return sessionFactory;
+    }
 }
